4a64a65c3a7052e15b9d3793a4e80783797d73a6
PaloDeEscoba <147647595+jesusmremon@users.noreply.github.com>
2023-11-13 15:03:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -31,9 +31,9 @@ serper_key = st.secrets['serper_key']
 open_key = st.secrets['open_key']
 browserless_key = st.secrets['browserless_key']
 
-# os.environ['OPEN_AI_KEY'] = open_key
+os.environ['OPEN_AI_KEY'] = open_key
 
-openai.api_key = open_key
+os.environ['OPEN_AI_KEY']
 
 
     
